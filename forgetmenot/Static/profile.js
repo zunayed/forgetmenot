1d8347747a3acbbd042ea3f85ca887d10d93340c
Zunayed Ali <zunayed@gmail.com>
2013-12-09 11:40:05 -0500

    added D3 support

--- forgetmenot/Static/profile.js
+++ forgetmenot/Static/profile.js
@@ -25,41 +25,94 @@ $(document).ready(function() {
         }).hide();
     });
 
-
+    //d3 chart
+    var w = 200;
+    var h = 200;
     var alive_num = $('.alive_num').attr('id');
     var dead_num = $('.dead_num').attr('id');
 
-    var chart_array = [];
+    var dataset =[alive_num, dead_num];
 
-    var label = "Alive - " + alive_num;
-    var value = alive_num;
-    var data1 = {
-        label: label,
-        data: value,
-        color: "#B894FF"
-    };
+    var outerRadius = w / 2;
+    var innerRadius = w / 3.5;
+    var arc = d3.svg.arc()
+                    .innerRadius(innerRadius)
+                    .outerRadius(outerRadius);
 
-    var label = "Dead - " + dead_num ;
-    var value = dead_num;
-    var data2 = {
-        label: label,
-        data: value,
-        color: "#33CCCC"
-    };
+    var pie = d3.layout.pie();
 
-    chart_array.push(data1, data2); 
+    //Easy colors accessible via a 10-step ordinal scale
+    var color = d3.scale.category10();
 
-    $.plot('#placeholder1', chart_array, {
-        series: {
-            pie: {
-                innerRadius: 0.5,
-                show: true
-            }
-        },
-        legend: {
-            position: "se",
-            container: $("#chartLegend")
-        }
-    });
+    //Create SVG element
+    var svg = d3.select(".d3")
+                .append("svg")
+                .attr("width", w)
+                .attr("height", h);
+
+    //Set up groups
+    var arcs = svg.selectAll("g.arc")
+                    .data(pie(dataset))
+                    .enter()
+                    .append("g")
+                    .attr("class", "arc")
+                    .attr("transform", "translate(" + outerRadius + "," + outerRadius + ")");
+
+    //Draw arc paths
+    arcs.append("path")
+        .attr("fill", function(d, i) {
+            return color(i);
+        })
+    .   attr("d", arc);
+
+    //Labels
+    arcs.append("text")
+        .attr("transform", function(d) {
+            return "translate(" + arc.centroid(d) + ")";
+        })
+        .attr("text-anchor", "middle")
+        .text(function(d) {
+            return d.value;
+        });
+
+
+
+
+    //Flot Donut chart
+    // var alive_num = $('.alive_num').attr('id');
+    // var dead_num = $('.dead_num').attr('id');
+
+    // var chart_array = [];
+
+    // var label = "Alive - " + alive_num;
+    // var value = alive_num;
+    // var data1 = {
+    //     label: label,
+    //     data: value,
+    //     color: "#B894FF"
+    // };
+
+    // var label = "Dead - " + dead_num ;
+    // var value = dead_num;
+    // var data2 = {
+    //     label: label,
+    //     data: value,
+    //     color: "#33CCCC"
+    // };
+
+    // chart_array.push(data1, data2); 
+
+    // $.plot('#placeholder1', chart_array, {
+    //     series: {
+    //         pie: {
+    //             innerRadius: 0.5,
+    //             show: true
+    //         }
+    //     },
+    //     legend: {
+    //         position: "se",
+    //         container: $("#chartLegend")
+    //     }
+    // });
 
 });
