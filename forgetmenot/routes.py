2b6a9b3aa5c066c75f6955f270b5936840bb37a4
Zunayed Ali <zunayed@gmail.com>
2014-08-03 22:42:58 -0400

    cleanup

--- forgetmenot/routes.py
+++ forgetmenot/routes.py
@@ -1,35 +1,37 @@
-from forgetmenot import app
 from flask import Flask, render_template, request, url_for, redirect, session
+
+from forgetmenot import app
 from models import db, User, soundcloud_tracks
 from form import SignupForm, SigninForm
 
 import soundcloud as sc
 import os
 
-#create soundcloud client object with app credentials
+# create soundcloud client object with app credentials
 client = sc.Client(client_id=os.environ["ID"],
-                    client_secret=os.environ["SECRET"],
-                    redirect_uri=os.environ["REDIRECT"])
+                   client_secret=os.environ["SECRET"],
+                   redirect_uri=os.environ["REDIRECT"])
 
 
 @app.route('/')
 def home():
-    #db.create_all()
+    # db.create_all()
     return render_template('home.html')
 
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
-    #intiate for signupForm class from form.py
+    # intiate for signupForm class from form.py
     form = SignupForm()
 
-    #form validation
+    # form validation
     if request.method == 'POST':
         if form.validate() is False:
             # print str(form.errors['email'])
             return render_template('signup.html', form=form)
         else:
-            new_user = User(form.firstname.data, form.lastname.data, form.email.data, form.password.data)
+            new_user = User(
+                form.firstname.data, form.lastname.data, form.email.data, form.password.data)
 
             db.session.add(new_user)
             db.session.commit()
@@ -78,9 +80,15 @@ def profile():
 
     if user:
         sc_tracks = soundcloud_tracks.query.filter_by(user_id=user.id).all()
-        data = [[item.artist, item.title, item.alive, item.url, item.track_id] for item in sc_tracks]
+        data = [[item.artist, item.title, item.alive, item.url, item.track_id]
+                for item in sc_tracks]
 
-        return render_template('profile.html', data=enumerate(data), num_alive=num_alive, num_dead=num_dead)
+        return render_template(
+            'profile.html',
+            data=enumerate(data),
+            num_alive=num_alive,
+            num_dead=num_dead
+        )
     else:
         return redirect(url_for('signin'))
 
@@ -99,12 +107,12 @@ def update():
     cloud_url_list = set(track.permalink_url for track in cloud_fav_list)
 
     for track in db_data - cloud_url_list:
-        #track that are not in your soundcloud fav any more and are going to be marked dead
+        # track that are not in your soundcloud fav any more and are going to be marked dead
         current_track = soundcloud_tracks.query.filter_by(url=track).first()
         current_track.alive = False
 
     for track in cloud_url_list - db_data:
-        #new tracks to add to database
+        # new tracks to add to database
         trk = [item for item in cloud_fav_list if item.permalink_url == track][0]
         new_track = soundcloud_tracks(
             trk.id,
@@ -122,13 +130,14 @@ def update():
 
 @app.route('/link_services')
 def link_services():
-    soundcloud_code = request.args.get('code')
-    access_token = client.exchange_token(code=soundcloud_code)
-    user = User.query.filter_by(email=session['email']).first()
-    print user
-    user.soundcloud_token = access_token.access_token
-    print access_token.access_token
-    db.session.commit()
+    if 'code' in request.args:
+        soundcloud_code = request.args.get('code')
+        access_token = client.exchange_token(code=soundcloud_code)
+        user = User.query.filter_by(email=session['email']).first()
+        user.soundcloud_token = access_token.access_token
+        db.session.commit()
+
+        return redirect(url_for('profile'))
 
     return redirect(url_for('profile'))
 
@@ -136,7 +145,7 @@ def link_services():
 @app.route('/profile/soundcloud')
 def soundcloud():
     user = User.query.filter_by(email=session['email']).first()
-    #check to see if token already exist in database
+    # check to see if token already exist in database
     if user.soundcloud_token:
         return redirect(url_for('profile'))
     else:
