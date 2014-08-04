9e4a711462053e8ef47a4a2b34fa4f7e5ad6f151
Zunayed Ali <zunayed@gmail.com>
2014-08-03 22:31:02 -0400

    foundations of next song

--- forgetmenot/Static/profile.js
+++ forgetmenot/Static/profile.js
@@ -1,7 +1,10 @@
 $(document).ready(function() {
     // Show or hide the sticky footer button
-    addRowHandlers()
-    
+    var table = document.getElementById("table");
+    var current_song_index = 0;
+    addRowHandlers(table);
+    nextSong(table);
+
     $(window).scroll(function() {
         if ($(this).scrollTop() > 100) {
             $('.go-top').fadeIn(200);
@@ -10,13 +13,11 @@ $(document).ready(function() {
         }
     });
 
-
     // Animate the scroll to top
     $('.go-top').click(function(event) {
         event.preventDefault();
         $('html, body').animate({scrollTop: 0}, 300);
     });
-
 
     //search box
     var $rows = $('.track_table tr');
@@ -69,15 +70,14 @@ $(document).ready(function() {
         }
     });
 
-    $('.progress').spin();
-    $.get('/profile/update', function(){
-        $('.progress').spin(false);
-    });
+    // $('.progress').spin();
+    // $.get('/profile/update', function(){
+    //     $('.progress').spin(false);
+    // });
 
 });
 
-function addRowHandlers() {
-    var table = document.getElementById("table");
+function addRowHandlers(table) {
     var rows = table.getElementsByTagName("tr");
     for (i = 0; i < rows.length; i++) {
         var currentRow = table.rows[i];
@@ -86,15 +86,28 @@ function addRowHandlers() {
             {
                 return function() { 
                     var cell = row.getElementsByTagName("th")[0];
-                    var id = cell.innerHTML;
                     var sc_id = row.id;
 
                     url = "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/" + sc_id
                     settings = '&amp;color=ff5500&amp;auto_play=true&amp;hide_related=true&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false'
-                    $('#sc_player iframe').attr('src', url + settings)
+                    $('#sc_player').attr('src', url + settings)
                 };
             };
-
         currentRow.onclick = createClickHandler(currentRow);
     }
 }
+
+function nextSong(table){
+    var widgetIframe = document.getElementById('sc_player'),
+        widget = SC.Widget(widgetIframe),
+        newSoundUrl = 'http://api.soundcloud.com/tracks/13692671';
+
+    widget.bind(SC.Widget.Events.READY, function() {
+        // load new widget
+        console.log("widget ready")
+        widget.bind(SC.Widget.Events.FINISH, function() {
+            console.log("finished")
+            table.rows[43].trigger()
+        });
+    });
+};
